de779278db450108799333e95b71df6572ec4575
Neil McHenry <neilmchenry@gmail.com>
2020-08-06 02:02:57 -0500

    Update index.js

--- electron-voice/src/store/index.js
+++ electron-voice/src/store/index.js
@@ -1,11 +1,24 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+import mutations from './mutations'
+import actions from './actions'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+  state: {
+  },
+  mutations,
+  actions,
+  getters: {
+  },
+  plugins: [vuexLocal.plugin],
+  strict: debug
+})
