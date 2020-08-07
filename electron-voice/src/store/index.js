dcbf0a5f606a9a1c64582d2f82b84cb91d53a7f9
Neil McHenry <neilmchenry@gmail.com>
2020-08-06 23:26:40 -0500

    Put in example state data

--- electron-voice/src/store/index.js
+++ electron-voice/src/store/index.js
@@ -16,13 +16,54 @@ export default new Vuex.Store({
   state: {
     loading: false,
     sending: false,
-    error: null,
-    user: [],
+    error: 'Relax! This is just a drill error message',
+    user: {
+      username: 'Jack',
+      name: 'Jack Sparrow'
+    },
     reconnect: false,
-    activeRoom: null,
-    rooms: [],
-    users: [],
-    messages: [],
+    activeRoom: {
+      id: '124'
+    },
+    rooms: [
+      {
+        id: '123',
+        name: 'Ships'
+      },
+      {
+        id: '124',
+        name: 'Treasure'
+      }
+    ],
+    users: [
+      {
+        username: 'Jack',
+        name: 'Jack Sparrow',
+        presence: 'online'
+      },
+      {
+        username: 'Barbossa',
+        name: 'Hector Barbossa',
+        presence: 'offline'
+      }
+    ],
+    messages: [
+      {
+        username: 'Jack',
+        date: '11/12/1644',
+        text: 'Not all treasure is silver and gold mate'
+      },
+      {
+        username: 'Jack',
+        date: '12/12/1644',
+        text: 'If you were waiting for the opportune moment, that was it'
+      },
+      {
+        username: 'Hector',
+        date: '12/12/1644',
+        text: 'You know Jack, I thought I had you figured out'
+      }
+    ],
     userTyping: null
   },
   getters: {
